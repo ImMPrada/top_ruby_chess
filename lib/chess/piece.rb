b6d98c3d86fcc217be3f7d34662546ec99ea03e9
immprada <im.mprada@gmail.com>
2022-11-03 16:25:55 -0500

    add Chess::Piece #possible_positions

--- lib/chess/piece.rb
+++ lib/chess/piece.rb
@@ -16,5 +16,18 @@ module Chess
 
       [row, column]
     end
+
+    def possible_positions(position_deltas)
+      position_deltas.map { |delta| possible_move(@position.coordinates, delta) }.compact
+    end
+
+    def possible_move(position, delta)
+      move_to = [position[0] + delta[0], position[1] + delta[1]]
+
+      return nil if move_to[0] < MIN_INDEX || move_to[0] > MAX_INDEX
+      return nil if move_to[1] < MIN_INDEX || move_to[1] > MAX_INDEX
+
+      move_to
+    end
   end
 end

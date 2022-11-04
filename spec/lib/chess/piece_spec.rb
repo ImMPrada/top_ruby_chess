8133b248dd606d2a204892f65b7f3197a8fb023d
immprada <im.mprada@gmail.com>
2022-11-04 12:11:52 -0500

    add Chess::Piece #initialize spec

--- spec/lib/chess/piece_spec.rb
+++ spec/lib/chess/piece_spec.rb
@@ -1,5 +1,21 @@
 require './spec/spec_helper'
 
 RSpec.describe Chess::Piece do
-  
+  let(:piece_symbol) { :K }
+  let(:piece_coordinates) { [0, 0] }
+  let(:piece) { described_class.new(piece_coordinates, piece_symbol) }
+
+  describe '#initialize' do
+    it 'initializes a piece with a position' do
+      expect(piece.instance_variable_get(:@position).coordinates).to be(piece_coordinates)
+    end
+
+    it 'initializes a piece with a symbol' do
+      expect(piece.instance_variable_get(:@symbol)).to be(piece_symbol)
+    end
+
+    it 'piece\'s position is a node type' do
+      expect(piece.instance_variable_get(:@position)).to be_a(Chess::Node)
+    end
+  end
 end

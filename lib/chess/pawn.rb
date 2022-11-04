464d1b15709f5c27813e596260e5259787a3e988
immprada <im.mprada@gmail.com>
2022-11-04 14:07:15 -0500

    add Chess::Pawn initial mockup

---
+++ lib/chess/pawn.rb
@@ -0,0 +1,26 @@
+require_relative 'chess'
+require_relative 'piece'
+require 'byebug'
+
+module Chess
+  class Pawn < Piece
+    POSITION_DELTAS = [[0, 1]].freeze
+    CAPTURE_MOVEMENTS = [[1, 1], [-1, 1]].freeze
+    SYMBOL = :P
+
+    def initialize(coordinates, team)
+      super(coordinates, SYMBOL, team)
+      @first_move = true
+    end
+
+    def move_to(position)
+      if @first_move
+        super(position, POSITION_DELTAS + [[0, 2]])
+      else
+        super(position, POSITION_DELTAS)
+      end
+
+      @first_move = false
+    end
+  end
+end

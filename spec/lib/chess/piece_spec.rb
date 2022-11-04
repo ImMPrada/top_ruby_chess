0b818c2fe8778e90abf1ff7a772b99e241fdbbac
immprada <im.mprada@gmail.com>
2022-11-04 12:12:14 -0500

    add Chess::Piece #move_to spec

--- spec/lib/chess/piece_spec.rb
+++ spec/lib/chess/piece_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe Chess::Piece do
       expect(piece.instance_variable_get(:@position)).to be_a(Chess::Node)
     end
   end
+
+  describe '#move_to' do
+    let(:right_targe_position) { 'c2' }
+    let(:wrong_target_position) { 'b1' }
+
+    it 'moves the piece to the right target position' do
+      position_deltas = [[1, 2], [1, -2]]
+      piece.move_to(right_targe_position, position_deltas)
+
+      expect(piece.instance_variable_get(:@position).coordinates).to eq(position_deltas[0])
+    end
+
+    it 'does not move the piece to the wrong target position' do
+      position_deltas = [[1, 2], [1, -2]]
+      piece.move_to(wrong_target_position, position_deltas)
+
+      expect(piece.instance_variable_get(:@position).coordinates).to eq(piece_coordinates)
+    end
+  end
 end

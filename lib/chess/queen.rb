b05fc06bbf597c38e164ace5e53bb4663f93cd3a
immprada <im.mprada@gmail.com>
2022-11-04 16:22:00 -0500

    add Chess::Queen initial mockup

---
+++ lib/chess/queen.rb
@@ -0,0 +1,36 @@
+require_relative 'chess'
+require_relative 'piece'
+require 'byebug'
+
+module Chess
+  class Queen < Piece
+    SYMBOL = :Q
+
+    def initialize(coordinates, team)
+      super(coordinates, SYMBOL, team)
+      @generated_deltas = generate_deltas
+      @capture_movements = @generated_deltas
+    end
+
+    def move_to(position)
+      super(position, @generated_deltas)
+    end
+
+    def generate_deltas
+      deltas = []
+
+      (1..7).each do |i|
+        deltas << [0, i]
+        deltas << [i, i]
+        deltas << [i, 0]
+        deltas << [i, -i]
+        deltas << [0, -i]
+        deltas << [-i, -i]
+        deltas << [-i, 0]
+        deltas << [-i, i]
+      end
+
+      deltas
+    end
+  end
+end

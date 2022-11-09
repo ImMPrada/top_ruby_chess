f8b3abb9156d34a4e7d7cdd87c0fc85a5599c3ec
immprada <im.mprada@gmail.com>
2022-11-09 14:15:14 -0500

    add Chess::Coordinates class

--- lib/chess/step.rb
+++ lib/chess/step.rb
@@ -1,10 +1,12 @@
+require_relative 'coordinates'
+
 module Chess
   class Step
     attr_reader :coordinates, :parent
 
     def initialize(coordinates, parent = nil)
       @parent = parent
-      @coordinates = coordinates
+      @coordinates = Coordinates.new(coordinates)
     end
   end
 end

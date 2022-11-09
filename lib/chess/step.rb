8ac5ad4c605491462813cd140140073439302ad1
immprada <im.mprada@gmail.com>
2022-11-09 14:16:10 -0500

    Chess::Step attributechange: @parent to @prev_step

--- lib/chess/step.rb
+++ lib/chess/step.rb
@@ -2,10 +2,10 @@ require_relative 'coordinates'
 
 module Chess
   class Step
-    attr_reader :coordinates, :parent
+    attr_reader :coordinates, :prev_step
 
-    def initialize(coordinates, parent = nil)
-      @parent = parent
+    def initialize(coordinates, prev_step = nil)
+      @prev_step = prev_step
       @coordinates = Coordinates.new(coordinates)
     end
   end

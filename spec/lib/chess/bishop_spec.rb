72ec1883e97f29f3d7fa138e4319cceb6c18511a
immprada <im.mprada@gmail.com>
2022-11-15 15:50:36 -0500

    update by rubocop

--- spec/lib/chess/bishop_spec.rb
+++ spec/lib/chess/bishop_spec.rb
@@ -7,30 +7,39 @@ RSpec.describe Chess::Bishop do
     it 'can move to a8' do
       expect(bishop.move_to('a8')).not_to be_nil
     end
+
     it 'can move to g8' do
       expect(bishop.move_to('g8')).not_to be_nil
     end
+
     it 'can move to c6' do
       expect(bishop.move_to('c6')).not_to be_nil
     end
+
     it 'can move to e6' do
       expect(bishop.move_to('e6')).not_to be_nil
     end
+
     it 'can move to c4' do
       expect(bishop.move_to('c4')).not_to be_nil
     end
+
     it 'can move to e4' do
       expect(bishop.move_to('e4')).not_to be_nil
     end
+
     it 'can move to a2' do
       expect(bishop.move_to('a2')).not_to be_nil
     end
+
     it 'can move to h1' do
       expect(bishop.move_to('h1')).not_to be_nil
     end
+
     it "can't move to c5" do
       expect(bishop.move_to('c5')).to be_nil
     end
+
     it "can't move to h5" do
       expect(bishop.move_to('h5')).to be_nil
     end
--- spec/lib/chess/king_spec.rb
+++ spec/lib/chess/king_spec.rb
@@ -7,27 +7,35 @@ RSpec.describe Chess::King do
     it 'can move to d6' do
       expect(king.move_to('d6')).not_to be_nil
     end
+
     it 'can move to e6' do
       expect(king.move_to('e6')).not_to be_nil
     end
+
     it 'can move to e5' do
       expect(king.move_to('e5')).not_to be_nil
     end
+
     it 'can move to e4' do
       expect(king.move_to('e4')).not_to be_nil
     end
+
     it 'can move to d4' do
       expect(king.move_to('d4')).not_to be_nil
     end
+
     it 'can move to c4' do
       expect(king.move_to('c4')).not_to be_nil
     end
+
     it 'can move to c5' do
       expect(king.move_to('c5')).not_to be_nil
     end
+
     it 'can move to c6' do
       expect(king.move_to('c6')).not_to be_nil
     end
+
     it "can't move to d7" do
       expect(king.move_to('d7')).to be_nil
     end

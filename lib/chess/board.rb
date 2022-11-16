5e1dba9116d8733176a0a8c1e3f0fe0a553924ce
immprada <im.mprada@gmail.com>
2022-11-16 15:26:20 -0500

    update Chess::Board class, adding #occuped_cells method

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -1,16 +1,46 @@
 require_relative 'chess'
+require_relative 'pieces/king'
+require_relative 'pieces/queen'
+require_relative 'pieces/bishop'
+require_relative 'pieces/knight'
+require_relative 'pieces/rook'
+require_relative 'pieces/pawn'
 
 module Chess
   class Board
     attr_reader :cells, :pieces
 
-    Cell = Struct.new(:name, :occuped, :team, :row, :column)
+    Cell = Struct.new(:algebraic_notation, :occuped, :occuped_by, :team, :coordinates_notation)
+
     Pieces = Struct.new(:team, :king, :queens, :bishops, :knights, :rooks, :pawns)
 
     def initialize
       @cells = {}
+      @pieces = nil
 
       generate_cells
+      put_pieces
+    end
+
+    def occuped_cells
+      occuped_cells = {
+        WHITE_TEAM => [],
+        BLACK_TEAM => []
+      }
+
+      COLUMNS.each do |column|
+        (MIN_INDEX..MAX_INDEX).each do |row_index|
+          row = row_index + 1
+          @cells[column.to_sym]
+
+          cell = @cells[column.to_sym][row]
+          next unless cell.occuped
+
+          occuped_cells[cell.team] << cell.coordinates_notation
+        end
+      end
+
+      occuped_cells
     end
 
     private
@@ -20,9 +50,63 @@ module Chess
         @cells[column.to_sym] = {}
         (MIN_INDEX..MAX_INDEX).each do |row_index|
           row = row_index + 1
-          @cells[column.to_sym][row] = Cell.new("#{column}#{row}", false, nil, row, column)
+          @cells[column.to_sym][row] = Cell.new("#{column}#{row}", false, nil, nil, [COLUMNS.index(column), row_index])
         end
       end
+    end
+
+    def put_pieces
+      @pieces = {
+        WHITE_TEAM => generate_pieces(WHITE_TEAM, 2),
+        BLACK_TEAM => generate_pieces(BLACK_TEAM, 7)
+      }
+
+      take_cells
+    end
+
+    def generate_pieces(team, row_of_pawns)
+      main_row = main_row(team, row_of_pawns)
+
+      Pieces.new(
+        team,
+        King.new("d#{main_row}", team),
+        [Queen.new("e#{main_row}", team)],
+        [Bishop.new("c#{main_row}", team), Bishop.new("f#{main_row}", team)],
+        [Knight.new("b#{main_row}", team), Knight.new("g#{main_row}", team)],
+        [Rook.new("a#{main_row}", team), Rook.new("h#{main_row}", team)],
+        (0..7).to_a.map { |column_index| Pawn.new("#{COLUMNS[column_index]}#{row_of_pawns}", team) }
+      )
+    end
+
+    def main_row(team, row_of_pawns)
+      return row_of_pawns + 1 if team == BLACK_TEAM
+
+      row_of_pawns - 1
+    end
+
+    def take_cells
+      take_cells_by_team(WHITE_TEAM)
+      take_cells_by_team(BLACK_TEAM)
+    end
+
+    # rubocop:disable Metrics/AbcSize
+    def take_cells_by_team(team)
+      take_cell_by(@pieces[team].king)
+      @pieces[team].queens.each { |queen| take_cell_by(queen) }
+      @pieces[team].bishops.each { |bishop| take_cell_by(bishop) }
+      @pieces[team].knights.each { |knight| take_cell_by(knight) }
+      @pieces[team].rooks.each { |rook| take_cell_by(rook) }
+      @pieces[team].pawns.each { |pawn| take_cell_by(pawn) }
+    end
+    # rubocop:enable Metrics/AbcSize
+
+    def take_cell_by(piece)
+      algebraic_notation = piece.position.algebraic
+      cell = @cells[algebraic_notation.column.to_sym][algebraic_notation.row]
+
+      cell.team = piece.team
+      cell.occuped = true
+      cell.occuped_by = piece
     end
   end
 end
--- spec/lib/chess/board_spec.rb
+++ spec/lib/chess/board_spec.rb
@@ -3,20 +3,68 @@ require 'spec_helper'
 RSpec.describe Chess::Board do
   subject(:board) { described_class.new }
 
-  describe 'when is created' do
+  describe '#occuped_cells' do
     let(:array_of_columns) { Chess::COLUMNS.map(&:to_sym) }
     let(:array_of_rows) { (1..8).to_a }
 
-    it 'stores a hash in @cells property' do
-      expect(board.cells).to be_a(Hash)
+    before do
+      board
     end
 
-    it 'stores a hash, with first level keys as columns names' do
-      expect(board.cells.keys).to match_array(array_of_columns)
+    it 'returns a hash of to kewys: each team, in eache key, a has of two keys: algebraic and coordinates' do
+      expect(board.occuped_cells[[Chess::WHITE_TEAM,
+                                  Chess::BLACK_TEAM].sample].class).to be(Array)
     end
 
-    it 'stores, at each column, a hash with first level keys as row names' do
-      expect(board.cells[Chess::COLUMNS.sample.to_sym].keys).to match_array(array_of_rows)
+    describe 'when board is created, occuped cells, are initial occuped cells' do
+      let(:white_initial_occuped_cells) do
+        [
+          [0, 0],
+          [1, 0],
+          [2, 0],
+          [3, 0],
+          [4, 0],
+          [5, 0],
+          [6, 0],
+          [7, 0],
+          [0, 1],
+          [1, 1],
+          [2, 1],
+          [3, 1],
+          [4, 1],
+          [5, 1],
+          [6, 1],
+          [7, 1]
+        ]
+      end
+      let(:black_initial_occuped_cells) do
+        [
+          [0, 7],
+          [1, 7],
+          [2, 7],
+          [3, 7],
+          [4, 7],
+          [5, 7],
+          [6, 7],
+          [7, 7],
+          [0, 6],
+          [1, 6],
+          [2, 6],
+          [3, 6],
+          [4, 6],
+          [5, 6],
+          [6, 6],
+          [7, 6]
+        ]
+      end
+
+      it 'returns algebraic notation of occuped cells for white team' do
+        expect(board.occuped_cells[Chess::WHITE_TEAM]).to match_array(white_initial_occuped_cells)
+      end
+
+      it 'returns algebraic notation of occuped cells for black team' do
+        expect(board.occuped_cells[Chess::BLACK_TEAM]).to match_array(black_initial_occuped_cells)
+      end
     end
   end
 end

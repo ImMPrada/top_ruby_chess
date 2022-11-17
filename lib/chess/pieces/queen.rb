111ea1e202f50d5e7bafb6d3fbd56a90cb73a1c0
immprada <im.mprada@gmail.com>
2022-11-16 23:05:27 -0500

    update Chess::Queen

--- lib/chess/pieces/queen.rb
+++ lib/chess/pieces/queen.rb
@@ -6,31 +6,48 @@ module Chess
   class Queen < Piece
     SYMBOL = :Q
 
+    Vector = Struct.new(:enable, :deltas)
+
     def initialize(coordinates, team)
       super(coordinates, SYMBOL, team)
-      @generated_deltas = generate_deltas
-      @capture_movements = @generated_deltas
+      @generated_deltas = {
+        v1: Vector.new(true, []),
+        v2: Vector.new(true, []),
+        v3: Vector.new(true, []),
+        v4: Vector.new(true, []),
+        v5: Vector.new(true, []),
+        v6: Vector.new(true, []),
+        v7: Vector.new(true, []),
+        v8: Vector.new(true, [])
+      }
+      @capture_movements = nil
+
+      generate_deltas
     end
 
-    def move_to(position_algebraic, capturing = false)
-      super(position_algebraic, capturing ? @capture_movements : @generated_deltas)
+    def move_to(position_algebraic, occuped_cells, capturing = false)
+      super(position_algebraic, capturing ? @capture_movements : @generated_deltas, occuped_cells)
     end
 
+    private
+
+    # rubocop:disable Metrics/AbcSize
     def generate_deltas
-      deltas = []
+      (MIN_INDEX..MAX_INDEX).each do |i|
+        next if i == MIN_INDEX
 
-      (1..7).each do |i|
-        deltas << [0, i]
-        deltas << [i, i]
-        deltas << [i, 0]
-        deltas << [i, -i]
-        deltas << [0, -i]
-        deltas << [-i, -i]
-        deltas << [-i, 0]
-        deltas << [-i, i]
+        @generated_deltas[:v1].deltas << [0, i]
+        @generated_deltas[:v2].deltas << [i, i]
+        @generated_deltas[:v3].deltas << [i, 0]
+        @generated_deltas[:v4].deltas << [i, -i]
+        @generated_deltas[:v5].deltas << [0, -i]
+        @generated_deltas[:v6].deltas << [-i, -i]
+        @generated_deltas[:v7].deltas << [-i, 0]
+        @generated_deltas[:v8].deltas << [-i, i]
       end
 
-      deltas
+      @capture_movements = @generated_deltas
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
--- spec/lib/chess/queen_spec.rb
+++ spec/lib/chess/queen_spec.rb
@@ -1,47 +1,86 @@
 require 'spec_helper'
 
 RSpec.describe Chess::Queen do
-  subject(:queen) { described_class.new('d5', Chess::WHITE_TEAM) }
+  subject(:queen) { described_class.new('e5', Chess::WHITE_TEAM) }
 
-  describe 'with a queen on d5' do
-    it 'can move to d8' do
-      expect(queen.move_to('d8')).not_to be_nil
+  let(:occuped_cells) do
+    {
+      Chess::WHITE_TEAM => [[4, 3], [4, 6], [7, 7]],
+      Chess::BLACK_TEAM => [[0, 4], [2, 6], [5, 4]]
+    }
+  end
+
+  describe 'with a queen on e5, and some occuped cells' do
+    it 'can move to e6' do
+      expect(queen.move_to('e6', occuped_cells)).not_to be_nil
     end
 
-    it 'can move to g8' do
-      expect(queen.move_to('g8')).not_to be_nil
-    end
-
-    it 'can move to e5' do
-      expect(queen.move_to('e5')).not_to be_nil
-    end
-
-    it 'can move to e4' do
-      expect(queen.move_to('e4')).not_to be_nil
-    end
-
-    it 'can move to d4' do
-      expect(queen.move_to('d4')).not_to be_nil
-    end
-
-    it 'can move to c4' do
-      expect(queen.move_to('c4')).not_to be_nil
+    it 'can move to c7' do
+      expect(queen.move_to('c7', occuped_cells)).not_to be_nil
     end
 
     it 'can move to a5' do
-      expect(queen.move_to('a5')).not_to be_nil
+      expect(queen.move_to('a5', occuped_cells)).not_to be_nil
     end
 
-    it 'can move to b7' do
-      expect(queen.move_to('b7')).not_to be_nil
+    it 'can move to b5' do
+      expect(queen.move_to('b5', occuped_cells)).not_to be_nil
     end
 
-    it "can't move to a1" do
-      expect(queen.move_to('a1')).to be_nil
+    it 'can move to d5' do
+      expect(queen.move_to('d5', occuped_cells)).not_to be_nil
     end
 
-    it "can't move to g6" do
-      expect(queen.move_to('g6')).to be_nil
+    it 'can move to a1' do
+      expect(queen.move_to('a1', occuped_cells)).not_to be_nil
+    end
+
+    it 'can move to c3' do
+      expect(queen.move_to('c3', occuped_cells)).not_to be_nil
+    end
+
+    it 'can move to g3' do
+      expect(queen.move_to('g3', occuped_cells)).not_to be_nil
+    end
+
+    it 'can move to f5' do
+      expect(queen.move_to('f5', occuped_cells)).not_to be_nil
+    end
+
+    it 'can move to g7' do
+      expect(queen.move_to('g7', occuped_cells)).not_to be_nil
+    end
+
+    it "can't move to e7" do
+      expect(queen.move_to('e7', occuped_cells)).to be_nil
+    end
+
+    it "can't move to b8" do
+      expect(queen.move_to('b8', occuped_cells)).to be_nil
+    end
+
+    it "can't move to a3" do
+      expect(queen.move_to('a3', occuped_cells)).to be_nil
+    end
+
+    it "can't move to d1" do
+      expect(queen.move_to('d1', occuped_cells)).to be_nil
+    end
+
+    it "can't move to e4" do
+      expect(queen.move_to('e4', occuped_cells)).to be_nil
+    end
+
+    it "can't move to e1" do
+      expect(queen.move_to('e1', occuped_cells)).to be_nil
+    end
+
+    it "can't move to h5" do
+      expect(queen.move_to('h5', occuped_cells)).to be_nil
+    end
+
+    it "can't move to h8" do
+      expect(queen.move_to('h8', occuped_cells)).to be_nil
     end
   end
 end

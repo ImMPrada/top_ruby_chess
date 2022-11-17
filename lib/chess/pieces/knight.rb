a864813508a1461ec646e5f8fc84f0ef3071df07
immprada <im.mprada@gmail.com>
2022-11-16 23:54:27 -0500

    update Chess::Knight

--- lib/chess/pieces/knight.rb
+++ lib/chess/pieces/knight.rb
@@ -4,16 +4,46 @@ require 'byebug'
 
 module Chess
   class Knight < Piece
-    POSITION_DELTAS = [[-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1]].freeze
-    CAPTURE_MOVEMENTS = POSITION_DELTAS
     SYMBOL = :N
 
-    def initialize(position_algebraic, team)
-      super(position_algebraic, SYMBOL, team)
+    Vector = Struct.new(:enable, :deltas)
+
+    def initialize(coordinates, team)
+      super(coordinates, SYMBOL, team)
+      @generated_deltas = {
+        v1: Vector.new(true, []),
+        v2: Vector.new(true, []),
+        v3: Vector.new(true, []),
+        v4: Vector.new(true, []),
+        v5: Vector.new(true, []),
+        v6: Vector.new(true, []),
+        v7: Vector.new(true, []),
+        v8: Vector.new(true, [])
+      }
+      @capture_movements = nil
+
+      generate_deltas
     end
 
-    def move_to(position_algebraic, capturing = false)
-      super(position_algebraic, capturing ? CAPTURE_MOVEMENTS : POSITION_DELTAS)
+    def move_to(position_algebraic, occuped_cells, capturing = false)
+      super(position_algebraic, capturing ? @capture_movements : @generated_deltas, occuped_cells)
     end
+
+    private
+
+    # rubocop:disable Metrics/AbcSize
+    def generate_deltas
+      @generated_deltas[:v1].deltas << [-1, 2]
+      @generated_deltas[:v2].deltas << [1, 2]
+      @generated_deltas[:v3].deltas << [2, 1]
+      @generated_deltas[:v4].deltas << [2, -1]
+      @generated_deltas[:v5].deltas << [1, -2]
+      @generated_deltas[:v6].deltas << [-1, -2]
+      @generated_deltas[:v7].deltas << [-2, -1]
+      @generated_deltas[:v8].deltas << [-2, 1]
+
+      @capture_movements = @generated_deltas
+    end
+    # rubocop:enable Metrics/AbcSize
   end
 end
--- spec/lib/chess/knight_spec.rb
+++ spec/lib/chess/knight_spec.rb
@@ -1,47 +1,46 @@
 require 'spec_helper'
 
 RSpec.describe Chess::Knight do
-  subject(:knight) { described_class.new('d5', Chess::WHITE_TEAM) }
+  subject(:knight) { described_class.new('e5', Chess::WHITE_TEAM) }
 
-  describe 'with a knight on d5' do
-    it 'can move to c7' do
-      expect(knight.move_to('c7')).not_to be_nil
+  let(:occuped_cells) do
+    {
+      Chess::WHITE_TEAM => [[3, 6], [6, 3]],
+      Chess::BLACK_TEAM => [[2, 6], [2, 3], [5, 2]]
+    }
+  end
+
+  describe 'with a knight on e5' do
+    it 'can move to f7' do
+      expect(knight.move_to('f7', occuped_cells)).not_to be_nil
     end
 
-    it 'can move to e7' do
-      expect(knight.move_to('e7')).not_to be_nil
+    it 'can move to c4' do
+      expect(knight.move_to('c4', occuped_cells)).not_to be_nil
     end
 
-    it 'can move to f6' do
-      expect(knight.move_to('f6')).not_to be_nil
+    it 'can move to d3' do
+      expect(knight.move_to('d3', occuped_cells)).not_to be_nil
     end
 
-    it 'can move to f4' do
-      expect(knight.move_to('f4')).not_to be_nil
+    it 'can move to f3' do
+      expect(knight.move_to('f3', occuped_cells)).not_to be_nil
     end
 
-    it 'can move to e3' do
-      expect(knight.move_to('e3')).not_to be_nil
+    it "can't move to d7" do
+      expect(knight.move_to('d7', occuped_cells)).to be_nil
     end
 
-    it 'can move to c3' do
-      expect(knight.move_to('c3')).not_to be_nil
+    it "can't move to c7" do
+      expect(knight.move_to('c7', occuped_cells)).to be_nil
     end
 
-    it 'can move to b4' do
-      expect(knight.move_to('b4')).not_to be_nil
+    it "can't move to c3" do
+      expect(knight.move_to('c3', occuped_cells)).to be_nil
     end
 
-    it 'can move to b6' do
-      expect(knight.move_to('b6')).not_to be_nil
-    end
-
-    it "can't move to b7" do
-      expect(knight.move_to('b7')).to be_nil
-    end
-
-    it "can't move to d3" do
-      expect(knight.move_to('d3')).to be_nil
+    it "can't move to g4" do
+      expect(knight.move_to('g4', occuped_cells)).to be_nil
     end
   end
 end

1a18838489386a0ec36d40fda30a246dbd0c516b
immprada <im.mprada@gmail.com>
2022-11-21 11:43:10 -0500

    update Chess::Pawn, deltas generated depends on pawn's team

--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -30,6 +30,11 @@ module Chess
     private
 
     def generate_deltas
+      return generate_white_deltas if @team == WHITE_TEAM
+      return generate_black_deltas if @team == BLACK_TEAM
+    end
+
+    def generate_white_deltas
       @generated_deltas[:v1].deltas = []
       @capture_movements[:v1].deltas = []
 
@@ -40,6 +45,19 @@ module Chess
       return unless @first_move
 
       @generated_deltas[:v1].deltas << [0, 2]
+    end
+
+    def generate_black_deltas
+      @generated_deltas[:v1].deltas = []
+      @capture_movements[:v1].deltas = []
+
+      @generated_deltas[:v1].deltas << [0, -1]
+      @capture_movements[:v1].deltas << [1, -1]
+      @capture_movements[:v1].deltas << [-1, -1]
+
+      return unless @first_move
+
+      @generated_deltas[:v1].deltas << [0, -2]
     end
   end
 end

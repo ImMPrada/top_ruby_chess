2ea1c9c6e77b9ef3f4b172637674f4da19d28f00
immprada <im.mprada@gmail.com>
2022-11-21 11:31:07 -0500

    add Chess::StringTransformServices to Chess::Board, helping on #to_s

---
+++ lib/chess/board/string_transform_services.rb
@@ -0,0 +1,19 @@
+module Chess
+  module StringTransformServices
+    def to_s
+      response = "     a  b  c  d  e  f  g  h \n"
+
+      (MIN_INDEX..MAX_INDEX).each do |row_index|
+        row_index = MAX_INDEX - row_index
+        response += "  #{row_index + 1} "
+        COLUMNS.each do |column|
+          response += @cells[column.to_sym][row_index].to_s
+        end
+
+        response += " #{row_index + 1}\n"
+      end
+
+      "#{response}     a  b  c  d  e  f  g  h \n"
+    end
+  end
+end

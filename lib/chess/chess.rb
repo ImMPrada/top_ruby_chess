d095a1fbf69f24f94a4bc7201bd249b037ede85b
immprada <im.mprada@gmail.com>
2022-11-21 11:44:51 -0500

    add constants to Chess modulus

--- lib/chess/chess.rb
+++ lib/chess/chess.rb
@@ -6,5 +6,18 @@ module Chess
   QUEEN_SIDE = :queen
   KING_SIDE = :king
 
+  ERR_KING_WILL_DIE = :king_will_die
+  ERR_EMPTY_ORIGIN_CELL = :empty_cell
+  ERR_WRONG_PIECE_AT_CELL = :wrong_piece
+  COMMIT_SUCCESS = :success
+
+  MOVEMENT_RESULTS_MESSAGES = {
+    king_will_die: "Cant't move to target cell, the king will die",
+    empty_cell: 'Origin cell is empty',
+    wrong_piece: 'Origin cell has another piece',
+
+    success: 'movement succesfully commited'
+  }.freeze
+
   COLUMNS = %w[a b c d e f g h].freeze
 end

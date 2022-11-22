846be875a5ffe1a190dc3292439dee46dfa7047e
immprada <im.mprada@gmail.com>
2022-11-22 13:02:43 -0500

    add Chess::CellsFillingServices

---
+++ lib/chess/board/cells_filling_services.rb
@@ -0,0 +1,17 @@
+module Chess
+  module CellsFillingServices
+    def occup_cells_by(team)
+      @pieces[team].to_a.each { |pieces| occup_by(pieces) }
+    end
+
+    def occup_by(pieces)
+      pieces.each { |queen| occup_cell_by(queen) }
+    end
+
+    def occup_cell_by(piece)
+      cell = @cells[piece.position.algebraic.column.to_sym][piece.position.coordinates.row]
+
+      cell.occup_by(piece)
+    end
+  end
+end
--- lib/chess/board/check_attacks_services.rb
+++ lib/chess/board/check_attacks_services.rb
@@ -17,19 +17,5 @@ module Chess
 
       !pieces_result.compact.empty?
     end
-
-    def occup_cells_by(team)
-      @pieces[team].to_a.each { |pieces| occup_by(pieces) }
-    end
-
-    def occup_by(pieces)
-      pieces.each { |queen| occup_cell_by(queen) }
-    end
-
-    def occup_cell_by(piece)
-      cell = @cells[piece.position.algebraic.column.to_sym][piece.position.coordinates.row]
-
-      cell.occup_by(piece)
-    end
   end
 end

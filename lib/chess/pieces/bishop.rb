ef425006fde4ba08d053065371aacf49e4d6e1d4
immprada <im.mprada@gmail.com>
2022-11-27 13:01:01 -0500

    update Chess::Pieces::Bishop

--- lib/chess/pieces/bishop.rb
+++ lib/chess/pieces/bishop.rb
@@ -1,51 +1,53 @@
 require_relative '../chess'
-require_relative 'piece'
+require_relative 'base_piece'
 require 'byebug'
 
 module Chess
-  class Bishop < Piece
-    SYMBOL = :B
+  module Pieces
+    class Bishop < BasePiece
+      SYMBOL = :B
+      TEXT = "\u265d".freeze
 
-    def initialize(coordinates, team)
-      super(coordinates, SYMBOL, team)
-      @generated_deltas = {
-        v1: Vector.new(true, []),
-        v2: Vector.new(true, []),
-        v3: Vector.new(true, []),
-        v4: Vector.new(true, [])
-      }
-      @capture_moves = nil
+      def initialize(coordinates, team)
+        super(coordinates, SYMBOL, team)
+        @generated_deltas = [
+          v1: Vector.new(true, []),
+          v2: Vector.new(true, []),
+          v3: Vector.new(true, []),
+          v4: Vector.new(true, [])
+        ]
 
-      generate_deltas
-    end
-
-    def move_to(position_algebraic, occuped_cells, capturing = false)
-      super(position_algebraic, capturing ? @capture_moves : @generated_deltas, occuped_cells)
-    end
-
-    def can_attack_to?(target_position_algebraic, occuped_cells)
-      can_move_to?(target_position_algebraic, @capture_moves, occuped_cells)
-    end
-
-    def to_s
-      super("\u265d")
-    end
-
-    private
-
-    # rubocop:disable Metrics/AbcSize
-    def generate_deltas
-      (MIN_INDEX..MAX_INDEX).each do |i|
-        next if i == MIN_INDEX
-
-        @generated_deltas[:v1].deltas << [i, i]
-        @generated_deltas[:v2].deltas << [i, -i]
-        @generated_deltas[:v3].deltas << [-i, -i]
-        @generated_deltas[:v4].deltas << [-i, i]
+        generate_deltas
       end
 
-      @capture_moves = @generated_deltas
+      def move_to(position_algebraic, occuped_cells)
+        super(position_algebraic, @generated_deltas, occuped_cells)
+      end
+
+      def can_attack_to?(target_position_algebraic, occuped_cells)
+        can_move_to?(target_position_algebraic, @capture_moves, occuped_cells)
+      end
+
+      def to_s
+        super(TEXT)
+      end
+
+      private
+
+      # rubocop:disable Metrics/AbcSize
+      def generate_deltas
+        (MIN_INDEX..MAX_INDEX).each do |i|
+          next if i == MIN_INDEX
+
+          @generated_deltas[0].deltas << [i, i]
+          @generated_deltas[1].deltas << [i, -i]
+          @generated_deltas[2].deltas << [-i, -i]
+          @generated_deltas[3].deltas << [-i, i]
+        end
+
+        @capture_moves = @generated_deltas
+      end
+      # rubocop:enable Metrics/AbcSize
     end
-    # rubocop:enable Metrics/AbcSize
   end
 end

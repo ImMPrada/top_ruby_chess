dfc84f950556791013f41c4e4d0291ea5845448d
immprada <im.mprada@gmail.com>
2022-12-02 14:34:44 -0500

    delete existingo code from Chess::Core::Move::Main

--- lib/chess/core/move/main.rb
+++ lib/chess/core/move/main.rb
@@ -23,28 +23,6 @@ module Chess
           @piece_moved = nil
           @piece_captured = nil
         end
-
-        def update_occuped_cells(occuped_cells_coordinates_by_teams)
-          @occuped_cells_coordinates_by_teams = occuped_cells_coordinates_by_teams
-        end
-
-        private
-
-        def set_origin_target_and_teams
-          @origin_cell = assign_cell(:origin_cell)
-          @target_cell = assign_cell(:target_cell)
-          @friends_team = @team_filter
-          @enemies_team = ([WHITE_TEAM, BLACK_TEAM] - [@friends_team]).first
-        end
-
-        def assign_cell(cell_type)
-          splitted_cell_string = @intention[cell_type].split('')
-          @cells[splitted_cell_string[0].to_sym][splitted_cell_string[1].to_i - 1]
-        end
-
-        def find_cell(algebraic)
-          @cells[algebraic.column.to_sym][algebraic.row - 1]
-        end
       end
     end
   end

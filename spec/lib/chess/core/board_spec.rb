cc87bb3bb3bb955fc88a77d635f22db80cb7b39a
immprada <im.mprada@gmail.com>
2022-12-01 21:14:13 -0500

    upgrade Chess::Core::Board specs

--- spec/lib/chess/core/board_spec.rb
+++ spec/lib/chess/core/board_spec.rb
@@ -3,9 +3,9 @@ require 'chess/core/board'
 require 'chess/core/cell'
 
 RSpec.describe Chess::Core::Board do
-  subject(:board) { described_class.create_and_occupy }
-
   describe '#create_and_occupy' do
+    subject(:board) { described_class.create_and_occupy }
+
     it 'creates an array at @cells' do
       expect(board.cells).to be_a(Array)
     end
@@ -69,6 +69,58 @@ RSpec.describe Chess::Core::Board do
 
     it 'creates 8 rooks for black team' do
       expect(board.pieces[Chess::BLACK_TEAM].find_pieces_of(:P).size).to be(8)
+    end
+  end
+
+  describe '#can_any_piece_move_to?' do
+    subject(:board) { described_class.new }
+
+    let(:cells) { board.cells }
+
+    before { board.generate_cells }
+
+    describe 'when cells are all released' do
+      it 'returns nil' do
+        expect(board.can_any_piece_move_to?(
+                 cells.sample.sample,
+                 cells,
+                 board.pieces
+               )).to be_nil
+      end
+    end
+
+    describe 'with a bishop at a1' do
+      let(:bishop) { Chess::Core::Pieces::Bishop.create_and_occupy(Chess::WHITE_TEAM, cells[0][0]) }
+
+      it 'returns true when a cell is at c3' do
+        expect(board.can_any_piece_move_to?(
+                 cells[2][2],
+                 cells,
+                 [bishop]
+               )).to be(true)
+      end
+
+      it 'returns false when a cell is at c4' do
+        expect(board.can_any_piece_move_to?(
+                 cells[3][2],
+                 cells,
+                 [bishop]
+               )).to be(false)
+      end
+
+      # rubocop:disable RSpec/NestedGroups
+      describe 'with also a rook at h4' do
+        let(:rook) { Chess::Core::Pieces::Rook.create_and_occupy(Chess::WHITE_TEAM, cells[3][7]) }
+
+        it 'returns false when a cell is at c4' do
+          expect(board.can_any_piece_move_to?(
+                   cells[3][2],
+                   cells,
+                   [bishop, rook]
+                 )).to be(true)
+        end
+      end
+      # rubocop:enable RSpec/NestedGroups
     end
   end
 end

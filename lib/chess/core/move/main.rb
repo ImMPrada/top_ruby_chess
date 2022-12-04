7542c236bff3d76b4db1f652a2517fbac1ca245c
immprada <im.mprada@gmail.com>
2022-12-03 23:01:09 -0500

    to rollback

--- lib/chess/core/move/main.rb
+++ lib/chess/core/move/main.rb
@@ -1,7 +1,9 @@
 require_relative '../chess'
-require_relative 'submit_services'
-require_relative 'commit_services'
-require_relative 'rollback_services'
+# require_relative 'submit_services'
+# require_relative 'commit_services'
+# require_relative 'rollback_services'
+require_relative '../../functional/cell_notation'
+require 'byebug'
 
 module Chess
   module Core
@@ -10,18 +12,24 @@ module Chess
         include SubmitServices
         include CommitServices
         include RollbackServices
+        include Chess::Functional::CellNotation
 
         attr_reader :king_position_string, :enemies_team, :piece_captured, :intention, :team_filter
 
-        def initialize(intention, board, team_filter)
-          @intention = intention
-          @cells = board.cells
-          @pieces = board.pieces
-          @symbol_filter = @intention[:symbol_filter].to_sym
+        # def initialize(intention, board, team_filter)
+        def initialize
+          # @intention = intention
+          # @cells = board.cells
+          # @pieces = board.pieces
+          # @symbol_filter = @intention[:symbol_filter].to_sym
 
-          @team_filter = team_filter
-          @piece_moved = nil
-          @piece_captured = nil
+          # @team_filter = team_filter
+          # @piece_moved = nil
+          # @piece_captured = nil
+        end
+
+        def execute
+          byebug
         end
       end
     end

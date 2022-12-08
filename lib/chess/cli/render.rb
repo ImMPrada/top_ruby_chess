8ae8c32afcb1fb8299dfa7259364a871bd3b284a
immprada <im.mprada@gmail.com>
2022-12-08 18:45:52 -0500

    upgrade Chess::CLI::Render to render the game's board

--- lib/chess/cli/render.rb
+++ lib/chess/cli/render.rb
@@ -9,7 +9,7 @@ module Chess
       include Chess::Core::Constants
 
       def initialize
-        @records_history = nil
+        @current_player = nil
       end
 
       def ask_for_prompt
@@ -19,32 +19,46 @@ module Chess
       def header(board)
         puts "\n"
         puts "\n"
-        puts board
+        puts board_to_string(board)
         puts "\n"
         puts "  \u00bb #{@current_player} \u00ab  ".black.on_red if @current_player == BLACK_TEAM
         puts "  \u00bb #{@current_player} \u00ab  ".light_white.on_light_red if @current_player == WHITE_TEAM
       end
 
-      def current_player(player)
+      def update_current_player(player)
         @current_player = player
       end
 
-      def show_record
-        puts "RECORD\n"
-        line = 0
+      private
 
-        historic_string = @records_history.map do |play_record|
-          line += 1
-          "#{line}. #{play_record}\n"
+      def board_to_string(board)
+        headers = "    a  b  c  d  e  f  g  h    \n".yellow
+
+        row = headers
+        (MAX_INDEX..MIN_INDEX).step(-1).each do |row_index|
+          row += " #{row_index + 1} ".yellow
+          row_of_cells = board.cells[row_index]
+          row_of_cells.each do |cell|
+            row += cell_to_string(cell)
+          end
+          row += " #{row_index + 1} \n".yellow
         end
 
-        puts historic_string.join
-
-        @records_history = nil
+        row += headers
       end
 
-      def update_records_history(records_history)
-        @records_history = records_history
+      def cell_to_string(cell)
+        return " #{piece_at(cell)} ".on_light_red if cell.fill_color == WHITE_TEAM
+
+        " #{piece_at(cell)} ".on_red if cell.fill_color == BLACK_TEAM
+      end
+
+      def piece_at(cell)
+        return ' ' unless cell.occupied?
+
+        return PIECE_STRING[cell.occupant.symbol].to_s.white if cell.occupant.team == WHITE_TEAM
+
+        PIECE_STRING[cell.occupant.symbol].to_s.black
       end
     end
   end
